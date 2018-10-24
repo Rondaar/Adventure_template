636056d4033c5eae23d4a84c7b4c58e1be3219b7
Rondaar <maciek.sozanski@gmail.com>
2018-10-25 00:49:12 +0200

    Added InputBehaviour and PlayerInput

---
+++ Adventure_template/Assets/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : InputBehaviour
+{	
+
+	void Update ()
+    {
+        ActionA = Input.GetButtonDown("Action");
+        ActionB = Input.GetButtonDown("UseItem");
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+
+        CheckForEvents();
+	}
+
+}
---
+++ Adventure_template/Assets/Scripts/Characters/Input/InputBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class InputBehaviour : MonoBehaviour {
+
+	public float Horizontal { get; protected set; }
+    public float Vertical { get; protected set; }
+    public bool ActionA { get; protected set; }
+    public bool ActionB { get; protected set; }
+
+    public event Action OnActionA = delegate { };
+    public event Action OnActionB = delegate { };
+
+    protected void CheckForEvents()
+    {
+        if (ActionA) OnActionA();
+        if (ActionB) OnActionB();
+    }
+}
