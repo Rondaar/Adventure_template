274b92fcb5625c86fdbee29239723fce56a1912e
Rondaar <maciek.sozanski@gmail.com>
2018-10-26 17:52:42 +0200

    Added MovementBehaviour script

---
+++ Adventure_template/Assets/Scripts/Characters/MovementBehaviour.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputBehaviour))]
+public class MovementBehaviour : MonoBehaviour {
+    [SerializeField]
+    float moveSpeed = 6;
+    [SerializeField]
+    PathCreator pathCreator;
+    [SerializeField]
+    float spacing = .1f;
+    [SerializeField]
+    float resolution = 1;
+
+    Vector2[] waypoints;
+    int currWaypointIndex = 0;
+
+    Rigidbody2D myRb;
+    InputBehaviour input;
+
+    private void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+        input = GetComponent<InputBehaviour>();
+        waypoints = pathCreator.path.CalculateEvenlySpacedPoints(spacing, resolution);
+        foreach (Vector2 p in waypoints)
+        {
+            GameObject g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            g.transform.position = p;
+            g.transform.localScale = Vector3.one * spacing * .5f;
+        }
+    }
+
+    private void Update()
+    {
+        Vector2 target = SetTarget();
+        transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed*Time.deltaTime);
+        CheckIfWaypointInRange();
+    }
+
+    private Vector2 SetTarget()
+    {
+        if (input.Horizontal<0)
+        {
+            return waypoints[currWaypointIndex];
+        }
+        else if (input.Horizontal>0)
+        {
+                return waypoints[currWaypointIndex + 1];
+        }
+        else
+        {
+            return transform.position;
+        }
+    }
+
+    private void CheckIfWaypointInRange()
+    {
+        if (input.Horizontal<0 && Vector2.Distance(waypoints[currWaypointIndex],transform.position)<.05f)
+        {
+            if (currWaypointIndex != 0)
+            {
+                currWaypointIndex--;
+            }
+        }
+        else if (input.Horizontal>0 && Vector2.Distance(waypoints[currWaypointIndex+1], transform.position) < .05f)
+        {
+            if (currWaypointIndex != waypoints.Length-2)
+            {
+                currWaypointIndex++;
+            }
+        }
+    }
+}
