fd35646125fa913ebe6df9414c9f28a4081d34bb
Rondaar <maciek.sozanski@gmail.com>
2018-10-29 23:52:01 +0100

    Added AnimatedMovePositionScript

---
+++ Adventure_template/Assets/Scripts/UI/AnimatedMovePosition.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedMovePosition : MonoBehaviour
+{
+
+    [SerializeField]
+    float duration;
+
+    [Space(10)]
+    [Header("Scale")]
+    [SerializeField]
+    AnimationCurve scaleCurve;
+    [Space(10)]
+    [Header("Position")]
+    [SerializeField]
+    AnimationCurve posCurve;
+
+    Vector3 startPos;
+    Vector3 startScale;
+    Vector3 endScale;
+    Vector3 endPos;
+    bool animCompleated = false;
+
+	void Start ()
+    {
+        InitializeVariables();
+    }
+	
+	void InitializeVariables()
+    {
+        startPos = Vector3.zero;
+        startScale = Vector3.zero;
+        endScale = transform.localScale;
+    }
+
+    public void TriggerAnimation(Vector3 _endPos = default(Vector3))
+    {
+        if (animCompleated)
+        {
+            StartCoroutine(MoveFromToPosition(endPos, startPos, endScale, startScale, duration));
+        }
+        else
+        {
+            endPos = _endPos;
+            StartCoroutine(MoveFromToPosition(startPos, endPos, startScale, endScale, duration));
+        }
+    }
+
+    IEnumerator MoveFromToPosition(Vector3 startPos,Vector3 endPos,Vector3 startScale, Vector3 endScale,float duration)
+    {
+        transform.localPosition = startPos;
+        transform.localScale = startScale;
+        float time = 0;
+        float perc = 0;
+        do
+        {
+            time += Time.unscaledDeltaTime;
+            perc = Mathf.Clamp01(time / duration);
+            Vector3 tempPos = Vector3.LerpUnclamped(startPos, endPos, posCurve.Evaluate(perc));
+            Vector3 tempScale = Vector3.LerpUnclamped(startScale, endScale, scaleCurve.Evaluate(perc));
+
+            transform.localPosition = tempPos;
+            transform.localScale = tempScale;
+            yield return null;
+
+        } while (perc < 1);
+    }
+}
